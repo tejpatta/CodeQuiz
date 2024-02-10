07b7b98c7b6c0d23081bc2883aaf2ee4dcc5a2be
Tejpatta <tejurawal@gmail.com>
2024-02-10 12:38:25 +0000

    added questions to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,56 @@
+// array of objects. Each object holds the questions, potential answers, and correct answer as attributes
+
+let questions = [
+    {
+        title: "What is JS?",
+        answers:["JavaScript", "Java", "Pyton", "C++"],
+        correct: "JavaScript",  
+    },{
+        title: "Which symbol represents a strict equality?",
+        answers: ["=","==","===","!=="],
+        correct: "==="
+    },{
+        title: "The condition in an if/else statement is enclosed with ______.",
+        answers: ["quotes","curly brackets","parentheses","square brackets"],
+        correct: "curly brackets"
+    },{
+        title: "What is the index of the first element in an array?",
+        answers:["-1","0","x","1"],
+        correct: "0"
+    },
+    {
+        title: "Which of these is a boolean value?",
+        answers:["yes", "false", "100","10.45"],
+        correct: "false"
+    },
+    {
+        title:"Arrays in JavaScript can be used to store:",
+        answers:["numbers and strings","other arrays","booleans","all of the above" ],
+        correct: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within _______ when being assigned to variables.",
+        answers:["commas", "curly brackets", "quotes", "parentheses"],
+        correct: "quotes"
+    },
+    {
+        title:"A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers:["JavaScript","terminal/bash","for loops","console.log"],
+        correct: "console.log"
+    },
+    {
+        title: "Which symbol is an assignment symbol?",
+        answers:["^","/","=","-"],
+        correct: "="
+    },
+    {
+        title: "Which symbol is a multiplication symbol?",
+        answers:["^","**","/","*"],
+        correct: "*"
+    },
+    {
+        title: "What does the % symbol mean?",
+        answers:["multiplication","percentage","remainder","division"],
+        correct: "remainder"
+    } 
+]
