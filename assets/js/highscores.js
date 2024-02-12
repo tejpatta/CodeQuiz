65afdbfe52a417ca72bdabbf8ffd32528d645841
Tejpatta <tejurawal@gmail.com>
2024-02-12 15:09:10 +0000

    add highscores.html and highscores logic

---
+++ assets/js/highscores.js
@@ -0,0 +1,29 @@
+let highscoresDisplay = document.getElementById("highscores");
+let clear = document.getElementById("clear");
+
+let prevScores = []
+
+renderScores()
+
+function renderScores(){  
+let storedScore = Object.entries(localStorage);
+    if (storedScore !== null) {
+    prevScores = Object.entries(localStorage);
+    } else{
+        highscoresDisplay.textContent = "no highscore!"
+    }
+getScores();
+}
+
+function getScores(){ 
+    highscoresDisplay.innerHTML = "";
+    for (var i=0; i<allScores.length; i++){
+        var li = document.createElement("li");
+        li.textContent = prevScores[i]
+        highscoresDisplay.appendChild(li)
+    }
+}
+
+clear.addEventListener("click", ()=>{ 
+    localStorage.clear();
+});
